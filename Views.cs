57695ce7eabba2c9e5c686f5abca00409d6af0e7
Michał Kossakowski <kossakowskimichal2002@gmail.com>
2024-10-08 18:30:20 +0200

    Dodaj pliki projektów.

---
+++ Views.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+import BookModel
+public class Views
+{
+	public BookAddView()
+	{
+        Console.WriteLine("Hello, World!");
+        AnsiConsole.Markup("[underline red]Hello[/] World!");
+
+        Console.WriteLine("Title:");
+        var title = Console.ReadLine();
+
+        Console.WriteLine("Author:");
+        var author = Console.ReadLine();
+
+        Console.WriteLine("Publish date:");
+        var publishDate = Convert.ToInt32(Console.ReadLine());
+
+        var book = new BookModel(title, author, publishDate);
+
+        book.Save();
+
+    }
+}
---
+++ kck_projekt1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Spectre.Console;
+using Model;
+
+namespace kck_projekt1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

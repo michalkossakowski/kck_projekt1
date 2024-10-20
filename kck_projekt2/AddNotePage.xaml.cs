1084fb3ee01d853f0151a148da3e875cfb22d7c2
michalkossakowski <141955539+michalkossakowski@users.noreply.github.com>
2024-10-20 16:11:42 +0200

    walidacja dla addnote i editnote

--- kck_projekt2/AddNotePage.xaml.cs
+++ kck_projekt2/AddNotePage.xaml.cs
@@ -1,24 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using kck_api.Controller;
+using MaterialDesignThemes.Wpf;
 
 namespace kck_projekt2
 {
-    /// <summary>
-    /// Logika interakcji dla klasy LoginPage.xaml
-    /// </summary>
     public partial class AddNotePage : UserControl
     {
         private MainWindow _mainWindow;
@@ -31,17 +19,52 @@ namespace kck_projekt2
 
         private async void AddNoteClick(object sender, RoutedEventArgs e)
         {
-            string titleValue = Title.Text;
             var selectedCategory = SelectedCategory.SelectedItem as ComboBoxItem;
-            string categoryValue = SelectedCategory.IsEnabled ? selectedCategory.Content.ToString() : CustomCategory.Text;
-            string contentValue = NoteContent.Text;
+            string category = CustomCategory.IsEnabled
+                ? CustomCategory.Text
+                : selectedCategory?.Content?.ToString() ?? "";
 
-            var noteController = NoteController.GetInstance();
+            if (string.IsNullOrWhiteSpace(Title.Text))
+            {
+                HintAssist.SetHelperText(Title, "Title cannot be empty");
+                Title.Foreground = new SolidColorBrush(Colors.Red);
+            }
 
-            var note = new NoteModel(_mainWindow.loggedUserId, titleValue, contentValue, categoryValue);
-            noteController.AddNote(note);
 
-            _mainWindow.contentControl.Content = new ActionMenuPage(_mainWindow);
+            if (category.Length == 0)
+            {
+                if (CustomCategory.IsEnabled)
+                {
+                    HintAssist.SetHelperText(CustomCategory, "Category cannot be empty");
+                    CustomCategory.Foreground = new SolidColorBrush(Colors.Red);
+
+                    HintAssist.SetHelperText(SelectedCategory, "Select category");
+                    SelectedCategory.Foreground = new SolidColorBrush(Colors.Black);
+                }
+                else
+                {
+                    HintAssist.SetHelperText(CustomCategory, "Enter custom category");
+                    CustomCategory.Foreground = new SolidColorBrush(Colors.Black);
+
+                    HintAssist.SetHelperText(SelectedCategory, "You must choose category or use custom");
+                    SelectedCategory.Foreground = new SolidColorBrush(Colors.Red);
+                }
+            }
+
+            if (NoteContent.Text.Length == 0)
+            {
+                HintAssist.SetHelperText(NoteContent, "Note content cannot be empty");
+                NoteContent.Foreground = new SolidColorBrush(Colors.Red);
+            }
+
+
+            if((!string.IsNullOrWhiteSpace(Title.Text)) && (category.Length > 0) && (NoteContent.Text.Length > 0))
+            {
+                var noteController = NoteController.GetInstance();
+                var note = new NoteModel(_mainWindow.loggedUserId, Title.Text, NoteContent.Text, category);
+                noteController.AddNote(note);
+                _mainWindow.contentControl.Content = new ActionMenuPage(_mainWindow);
+            }
         }
 
         private void BackClick(object sender, RoutedEventArgs e)
@@ -53,6 +76,46 @@ namespace kck_projekt2
         {
             CustomCategory.IsEnabled = !CustomCategory.IsEnabled;
             SelectedCategory.IsEnabled = !SelectedCategory.IsEnabled;
+        }
+
+
+        private void Title_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Title.Text.Length > 0)
+            {
+                HintAssist.SetHelperText(Title, "Enter note title");
+                Title.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void SelectedCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (SelectedCategory.SelectedItem != null)
+            {
+                HintAssist.SetHelperText(SelectedCategory, "Select category");
+                SelectedCategory.Foreground = new SolidColorBrush(Colors.Black);
+
+                HintAssist.SetHelperText(CustomCategory, "Enter custom category");
+                CustomCategory.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void CustomCategory_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (CustomCategory.Text.Length > 0)
+            {
+                HintAssist.SetHelperText(CustomCategory, "Enter custom category");
+                CustomCategory.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void NoteContent_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (NoteContent.Text.Length > 0)
+            {
+                HintAssist.SetHelperText(NoteContent, "");
+                NoteContent.Foreground = new SolidColorBrush(Colors.Black);
+            }
         }
     }
 }

0eb376d05a9f1e23c4cea7b6123088cfbae3a6a5
InterfectoremCubiculum <eryksliwowskic@gmail.com>
2025-03-02 16:40:40 +0100

    - mvvm chart dla statystyk dodany

---
+++ kck_projekt2/ViewModels/TopNavMenuViewModel.cs
@@ -0,0 +1,26 @@
+﻿using kck_projekt2.Charts;
+using System.Windows.Input;
+
+namespace kck_projekt2.ViewModels
+{
+    public class TopNavMenuViewModel: BaseViewModel
+    {
+
+        private readonly MainWindow _mainWindow;
+
+        public ICommand OpenNotesInMonthPageCommand { get; }
+        public ICommand OpenNotesByCategoryCommand { get; }
+
+        public TopNavMenuViewModel(MainWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+
+            OpenNotesInMonthPageCommand = new RelayCommand(OpenNotesInMonthPage);
+            OpenNotesByCategoryCommand = new RelayCommand(OpenNotesByCategoryPage);
+
+        }
+
+        private void OpenNotesInMonthPage() => _mainWindow.contentControl.Content = new NotesInMonthPage(_mainWindow);
+        private void OpenNotesByCategoryPage() => _mainWindow.contentControl.Content = new NotesByCategoryPage(_mainWindow);
+    }
+}

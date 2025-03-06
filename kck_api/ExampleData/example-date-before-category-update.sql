7ac3a462b920d6e61a105499791c4068ce452a55
michalkossakowski <141955539+michalkossakowski@users.noreply.github.com>
2025-03-06 22:16:30 +0100

    more example data

--- kck_api/ExampleData/example-date-before-category-update.sql
+++ kck_api/ExampleData/example-date-before-category-update.sql
@@ -35,3 +35,44 @@ VALUES
 (1, 'Ćwiczenia jogi', 'Rozpocząć poranną jogę', '2025-02-28', 6),
 (1, 'Książki must', 'Lista książek do przeczytania', '2025-03-02', 7),
 (1, 'Nowy telefon', 'Porównać modele przed zakupem', '2025-03-05', 9);
+
+INSERT INTO [kck_projektDb].[dbo].[Notes] ([AuthorId], [Title], [Content], [ModifiedDate], [CategoryId])
+VALUES 
+-- 2020
+(1, 'Nowy Rok 2020', 'Cele na nowy rok', '2020-01-01', 2),
+(1, 'Budżet luty', 'Analiza wydatków', '2020-02-10', 8),
+(1, 'Ćwiczenia plan', 'Ustalić grafik ćwiczeń', '2020-03-15', 6),
+(1, 'Zakupy lato', 'Letnie ubrania i akcesoria', '2020-06-20', 3),
+(1, 'Wakacje plan', 'Zaplanować wyjazd wakacyjny', '2020-07-05', 4),
+(1, 'Powrót do szk.', 'Zakup książek i przyborów', '2020-08-25', 10),
+(1, 'Jesienne menu', 'Nowe przepisy na jesień', '2020-10-10', 2),
+(1, 'Święta 2020', 'Lista prezentów świątecznych', '2020-12-05', 2),
+
+-- 2021
+(1, 'Cele 2021', 'Plany na nowy rok', '2021-01-02', 2),
+(1, 'Auto serwis', 'Zaplanować przegląd auta', '2021-04-18', 2),
+(1, 'Sportowy cel', 'Biegać 3x w tygodniu', '2021-05-07', 6),
+(1, 'Kurs online', 'Znaleźć kurs programowania', '2021-09-14', 10),
+(1, 'Kino jesień', 'Sprawdzić premiery filmowe', '2021-11-22', 7),
+
+-- 2022
+(1, 'Nowy Rok 2022', 'Ustalić postanowienia', '2022-01-03', 2),
+(1, 'Finanse luty', 'Analiza oszczędności', '2022-02-28', 8),
+(1, 'Bieg na 5km', 'Przygotować się do zawodów', '2022-06-10', 6),
+(1, 'Podróż góry', 'Sprawdzić szlaki w Tatrach', '2022-08-05', 4),
+(1, 'Jesień plany', 'Zrobić listę celów na Q4', '2022-09-20', 1),
+(1, 'Prezenty 2022', 'Kupić świąteczne upominki', '2022-12-10', 2),
+
+-- 2023
+(1, 'Ćwiczenia luty', 'Rozpisać nowy plan treningowy', '2023-02-08', 6),
+(1, 'Podróż maj', 'Plan na długi weekend', '2023-05-15', 4),
+(1, 'Budżet lipiec', 'Przeanalizować wydatki wakacyjne', '2023-07-25', 8),
+(1, 'Książki lato', 'Lista do przeczytania', '2023-08-11', 7),
+(1, 'Kino zimowe', 'Sprawdzić grudniowe premiery', '2023-12-01', 7),
+
+-- 2024
+(1, 'Nowy Rok 2024', 'Cele na rok', '2024-01-05', 2),
+(1, 'Dieta plan', 'Przygotować zdrowe menu', '2024-03-22', 5),
+(1, 'Nowy telefon', 'Znaleźć najlepszy model', '2024-06-14', 9),
+(1, 'Sport jesień', 'Zapisać się na siłownię', '2024-09-09', 6),
+(1, 'Zakupy Black', 'Lista na Black Friday', '2024-11-27', 3),
